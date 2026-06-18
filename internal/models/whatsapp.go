e3dd6915f72a2a6339c0ed5e19bf8c6985a5b702
muneeffaisalav <muneeffaisalav@gmail.com>
2026-06-18 13:16:02 +0000

    json incoming format change

--- internal/models/whatsapp.go
+++ internal/models/whatsapp.go
@@ -1,7 +1,6 @@
 package models
 
 // WhatsappWebhook is the top-level struct for a WhatsApp webhook
-
 type WhatsappWebhook struct {
 	Entry []struct {
 		Changes []struct {
@@ -13,16 +12,29 @@ type WhatsappWebhook struct {
 }
 
 // Message is a single message from a WhatsApp webhook
-
 type Message struct {
-	From        string `json:"from"`
-	ID          string `json:"id"`
-	Timestamp   string `json:"timestamp"`
-	Interactive struct {
-		Type       string `json:"type"`
-		ButtonReply struct {
-			ID    string `json:"id"`
-			Title string `json:"title"`
-		} `json:"button_reply"`
-	} `json:"interactive"`
+	From        string       `json:"from"`
+	ID          string       `json:"id"`
+	Timestamp   string       `json:"timestamp"`
+	Type        string       `json:"type"`
+	Button      *Button      `json:"button,omitempty"`
+	Interactive *Interactive `json:"interactive,omitempty"`
+}
+
+// Button represents a button in a WhatsApp message
+type Button struct {
+	Payload string `json:"payload"`
+	Text    string `json:"text"`
+}
+
+// Interactive represents an interactive message in WhatsApp
+type Interactive struct {
+	Type        string      `json:"type"`
+	ButtonReply ButtonReply `json:"button_reply"`
+}
+
+// ButtonReply is the reply from a button in an interactive message
+type ButtonReply struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
 }
