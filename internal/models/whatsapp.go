bc34ff3673fc1de7738a034de0b2760bba8efa6e
muneeffaisalav <muneeffaisalav@gmail.com>
2026-06-18 13:28:22 +0000

    webhook issue fix

--- internal/models/whatsapp.go
+++ internal/models/whatsapp.go
@@ -13,27 +13,21 @@ type WhatsappWebhook struct {
 
 // Message is a single message from a WhatsApp webhook
 type Message struct {
-	From        string       `json:"from"`
-	ID          string       `json:"id"`
-	Timestamp   string       `json:"timestamp"`
-	Type        string       `json:"type"`
-	Button      *Button      `json:"button,omitempty"`
-	Interactive *Interactive `json:"interactive,omitempty"`
+	From        string               `json:"from"`
+	ID          string               `json:"id"`
+	Timestamp   string               `json:"timestamp"`
+	Type        string               `json:"type"`
+	Button      *Button              `json:"button,omitempty"`
+	Interactive *IncomingInteractive `json:"interactive,omitempty"`
 }
 
-// Button represents a button in a WhatsApp message
-type Button struct {
-	Payload string `json:"payload"`
-	Text    string `json:"text"`
-}
-
-// Interactive represents an interactive message in WhatsApp
-type Interactive struct {
+// IncomingInteractive represents the interactive part of an incoming message from WhatsApp
+type IncomingInteractive struct {
 	Type        string      `json:"type"`
 	ButtonReply ButtonReply `json:"button_reply"`
 }
 
-// ButtonReply is the reply from a button in an interactive message
+// ButtonReply represents a button reply from WhatsApp
 type ButtonReply struct {
 	ID    string `json:"id"`
 	Title string `json:"title"`
